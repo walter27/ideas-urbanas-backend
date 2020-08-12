2cacbe3b3ae7bb9f315edaf0435a85dff3a9c564
walter_rz <walter.remache@fractalsoft.ec>
2020-08-11 19:08:21 -0500

    Compartir imagen v5

--- controllers/charts.js
+++ controllers/charts.js
@@ -40,15 +40,10 @@ function saveCharts(req, res) {
 
 function shareChart(req, res) {
 
-    console.log(req);
 
     let share = `${req.body.name}_${new Date().getMilliseconds()}`;
     let pathImage = path.resolve(__dirname, `../${req.body.type}/image.png`)
     let pathImageShare = path.resolve(__dirname, `../share/${share}.png`);
-
-    console.log(pathImage);
-    console.log(pathImageShare);
-
     fs.copyFileSync(pathImage, pathImageShare);
 
     res.json({
